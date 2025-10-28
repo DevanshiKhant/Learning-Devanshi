559eea2b95c4f8ee04c5c88eda344b08a4a29a0d
unknown <devanshi.codesymphony@gmail.com>
2025-10-28 16:00:49 +0530

    comment changes apply

--- expressjs/index.js
+++ expressjs/index.js
@@ -3,23 +3,29 @@ const app = express()
 const  data = require('./data.json')
 const port = 3000
 
-
-
 app.get('/findall', (req, res) => {
   const userall = data;
-  res.json(userall);
+  res.json({meassage : "all user are found" , user : userall});
 });
 
-app.get('/:id',(req,res) => {
+app.get('/:id',(req,res) => { 
     const id = parseInt(req.params.id);
     const user = data.find(u =>u.id === id);
     if(user)
     {
-        res.json(user)
+        res.json(user);
     }
     else
     {
-      res.status(404).send('User not found');
+
+      res.status(404).json({
+        sucess:false,
+        statuscode:404,
+        meassage:'user not found',
+        error:{
+          details : `the requested id ${id} could not be found.`
+        }
+      });
     }
 
 
